3d653a223f0186f0581b0b691080c4174491481c
darwinj94 <darwinj94@hotmail.com>
2020-06-23 13:58:51 -0500

    first commit

---
+++ src/app/table/ngtable/ngtable.ts
@@ -0,0 +1,7 @@
+export class Table {
+    Id: number;
+    public Name: string;
+    public UserName: string;
+    public Email: string;
+    imagePath:string
+  }
---
+++ src/app/widgets/data/widget-data.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+import { NgbProgressbarConfig } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  templateUrl: 'widget-data.component.html'
+})
+export class WidgetdataComponent {}
