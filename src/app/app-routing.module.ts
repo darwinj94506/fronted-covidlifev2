ff6330821ceb3901b2ea3187085050486df28791
darwinj94506 <darwinj94@hotmail.com>
2021-04-09 09:32:21 -0500

    descomenté los guards del login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { FullComponent } from './layouts/full/full.component';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { InitComponent } from './init/init.component';
@@ -11,7 +10,7 @@ import { InfoHospitalComponent } from './shared/info-hospital/info-hospital.comp
 export const Approutes: Routes = [
 	{
 		path: '',
-		// canActivate:[ LoginGuard ],
+		canActivate:[ LoginGuard ],
 		component: FullComponent,
 		children: [
 			{ path: '', redirectTo: '/dashboard/dashboard1', pathMatch: 'full' },
@@ -80,7 +79,7 @@ export const Approutes: Routes = [
 			},
 			{ 
 				path: 'paciente', 
-				// canActivate:[ RoleGuardService ],
+				canActivate:[ RoleGuardService ],
 				data: { expectedRole: RolesUserEnum.PACIENTE }, 
 				loadChildren: () => import('./modules/patient/patient.module').then(m => m.PatientModule) 
 			},
